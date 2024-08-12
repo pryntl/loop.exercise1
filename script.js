d5f72778b8b1f82c581d8d9ff3c4ae6115ed3aa6
pryntl <pryntl2007@gmail.com>
2024-08-12 16:45:48 +0330

    update [java script] : comment
    added comments to explain the code.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+//defining an array which includes unsorted numbers.
 let numbers = [30, 2, 56, 92, 8];
+//printing the mentioned array into console.
 console.log("unsorted array:", numbers);
+//making a function that sorts the array when it's mentioned.
 function sortArray(sort) {
+    //making a loop which at first compares the index 1 and 0 of the array. and then the index 2 and 1 until it reaches the last index which means i-1.
   for (let i = 1; i < numbers.length; i++) {
     for (let j = 0; j < i; j++) {
       if (numbers[i] < numbers[j]) {
@@ -10,6 +14,8 @@ function sortArray(sort) {
       }
     }
   }
+  //at the end returning the sorted array.
   return numbers;
 }
+//testing the loop.
 console.log("sorted array:", sortArray(numbers));
