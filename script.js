4d41900de78dd4ed1cd0e710224469bee4613235
pryntl <pryntl2007@gmail.com>
2024-08-12 16:39:19 +0330

    feat [java script]
    added a function which sort an array, using for loops.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+let numbers = [30, 2, 56, 92, 8];
+console.log("unsorted array:", numbers);
+function sortArray(sort) {
+  for (let i = 1; i < numbers.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (numbers[i] < numbers[j]) {
+        let x = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = x;
+      }
+    }
+  }
+  return numbers;
+}
+console.log("sorted array:", sortArray(numbers));
